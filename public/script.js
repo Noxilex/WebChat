18a1d3b6374b09b9e601d883c10346706a414bb1
Noxilex <dhoop59@hotmail.fr>
2019-05-19 17:01:56 +0200

    Updated interface

--- public/script.js
+++ public/script.js
@@ -1,1 +1,57 @@
+class Message {
+    constructor(content="", user = new User()){
+      this.dateCreated = new Date();
+      this.dateEdited;
+      this.content = content;
+      this.user = user;
+    }
+}
+  
+class User {
+    constructor(name="anonymous") {
+        this.name = name;
+    }
+}
+
 var socket = io();
+var chatTextArea = document.querySelector("#chat-send");
+var messages = [];
+
+chatTextArea.addEventListener("keypress", (event) => {
+    if(event.key == "Enter"){
+        console.log(chatTextArea)
+        addMessage(new Message(chatTextArea.value));
+        chatTextArea.value = "";
+    }
+})
+
+
+function updateMessages(messages){
+
+}
+
+function addMessage(message, user){
+    let chat = document.querySelector("#chat-history");
+    let messageContent = document.createElement("li");
+    let userDom = document.createElement("span");
+    let messageDom = document.createElement("span");
+
+    if(!user){
+        user = message.user;
+    }
+
+    console.log(user)
+
+    userDom.classList.add("username");
+    messageDom.classList.add("message");
+
+    userDom.innerText = user.name;
+    messageDom.innerText = message.content;
+
+    messageContent.appendChild(userDom);
+    messageContent.appendChild(messageDom);
+
+    chat.appendChild(messageContent);
+}
+
+
--- server.js
+++ server.js
@@ -15,11 +15,14 @@ api.get("/", function(request, response) {
   response.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-io.on('connection', () => {
+let users = [];
+io.on('connection', (socket) => {
+    socket.emit('new_user', users);
     console.log("A new user has connected to the server");
 });
+
 // Starts the server.
 server.listen(5000, "0.0.0.0", function() {
     console.log("Started server on port 5000");
-  });
-  
+});
+ 
